de1a75407af1fb6ac487fa13a337b9d1be3c6cd9
Jose Octavio Molina Nava <jose.octavio@berkeley.edu>
2021-08-17 19:12:07 -0500

    adding footer

---
+++ src/components/Footer/index.js
@@ -0,0 +1,91 @@
+import React from 'react'
+
+
+const Footer = () => {
+    return (
+        <FooterContainer>
+            <FooterWrap>
+                <FooterLinksContainer>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>
+                                About me
+                            </FooterLinkTitle>
+                            <FooterLink a="www.facebook.com">
+                                Github
+                            </FooterLink>
+                            <FooterLink a="www.facebook.com">
+
+                            </FooterLink>
+                            <FooterLink a="www.facebook.com">
+                                Linkedin
+                            </FooterLink>
+                            <FooterLink a="www.facebook.com">
+                                Facebook
+                            </FooterLink>
+                            
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>
+                                About me
+                            </FooterLinkTitle>
+                            <FooterLink a="www.facebook.com">
+                                Github
+                            </FooterLink>
+                            <FooterLink a="www.facebook.com">
+
+                            </FooterLink>
+                            <FooterLink a="www.facebook.com">
+                                Linkedin
+                            </FooterLink>
+                            <FooterLink a="www.facebook.com">
+                                Facebook
+                            </FooterLink>
+                            
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                    <FooterLinksWrapper>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>
+                                About me
+                            </FooterLinkTitle>
+                            <FooterLink a="www.facebook.com">
+                                Github
+                            </FooterLink>
+                            <FooterLink a="www.facebook.com">
+
+                            </FooterLink>
+                            <FooterLink a="www.facebook.com">
+                                Linkedin
+                            </FooterLink>
+                            <FooterLink a="www.facebook.com">
+                                Facebook
+                            </FooterLink>
+                            
+                        </FooterLinkItems>
+                        <FooterLinkItems>
+                            <FooterLinkTitle>
+                                About me
+                            </FooterLinkTitle>
+                            <FooterLink a="www.facebook.com">
+                                Github
+                            </FooterLink>
+                            <FooterLink a="www.facebook.com">
+
+                            </FooterLink>
+                            <FooterLink a="www.facebook.com">
+                                Linkedin
+                            </FooterLink>
+                            <FooterLink a="www.facebook.com">
+                                Facebook
+                            </FooterLink>
+                            
+                        </FooterLinkItems>
+                    </FooterLinksWrapper>
+                </FooterLinksContainer>
+            </FooterWrap>
+        </FooterContainer>
+    )
+}
+
+export default Footer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,11 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import HeroSection from '../components/HeroSection'
 import InfoSection from '../components/InfoSection'
 import { homeObjOne, homeObjTwo, homeObjThree } from '../components/InfoSection/Data'
-import Navbar from '../components/Navbar/index'
-import Sidebar from '../components/Sidebar/index'
-import Services from '../components/Services/index'
+import Navbar from '../components/Navbar/index';
+import Sidebar from '../components/Sidebar/index';
+import Services from '../components/Services/index';
+import Footer from '../components/Footer';
+
 
 const Home = () => {
     const [isOpen, setIsOpen] = useState(false)
@@ -24,7 +26,7 @@ const Home = () => {
             <InfoSection {...homeObjTwo} />
             <Services/>
             <InfoSection {...homeObjThree} />
-
+            <Footer/>
         </Router>
 
     )
