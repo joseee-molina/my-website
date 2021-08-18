8cdfb9f959eecafb2644b30a6f79fe80a74db98f
Jose Octavio Molina Nava <jose.octavio@berkeley.edu>
2021-08-17 19:51:16 -0500

    adding footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,20 @@
 import React from 'react'
+import { FooterContainer,
+     FooterWrap,
+      FooterLinksContainer,
+FooterLinksWrapper,
+FooterLinkItems,
+FooterLinkTitle,
+FooterLink,
+SocialIconLink,
+SocialLogo,
+SocialIcons,
+SocialMedia,
+SocialMediaWrap,
+WebsiteRights,
+ } from './FooterElements';
 
+import {FaFacebook, FaLinkedin, FaInstagram, FaGithub} from 'react-icons/fa'
 
 const Footer = () => {
     return (
@@ -11,16 +26,16 @@ const Footer = () => {
                             <FooterLinkTitle>
                                 About me
                             </FooterLinkTitle>
-                            <FooterLink a="www.facebook.com">
+                            <FooterLink to="www.facebook.com">
                                 Github
                             </FooterLink>
-                            <FooterLink a="www.facebook.com">
+                            <FooterLink to="www.facebook.com">
 
                             </FooterLink>
-                            <FooterLink a="www.facebook.com">
+                            <FooterLink to="www.facebook.com">
                                 Linkedin
                             </FooterLink>
-                            <FooterLink a="www.facebook.com">
+                            <FooterLink to="www.facebook.com">
                                 Facebook
                             </FooterLink>
                             
@@ -29,16 +44,16 @@ const Footer = () => {
                             <FooterLinkTitle>
                                 About me
                             </FooterLinkTitle>
-                            <FooterLink a="www.facebook.com">
+                            <FooterLink to="www.facebook.com">
                                 Github
                             </FooterLink>
-                            <FooterLink a="www.facebook.com">
+                            <FooterLink to="www.facebook.com">
 
                             </FooterLink>
-                            <FooterLink a="www.facebook.com">
+                            <FooterLink to="www.facebook.com">
                                 Linkedin
                             </FooterLink>
-                            <FooterLink a="www.facebook.com">
+                            <FooterLink to="www.facebook.com">
                                 Facebook
                             </FooterLink>
                             
@@ -49,16 +64,16 @@ const Footer = () => {
                             <FooterLinkTitle>
                                 About me
                             </FooterLinkTitle>
-                            <FooterLink a="www.facebook.com">
+                            <FooterLink to="www.facebook.com">
                                 Github
                             </FooterLink>
-                            <FooterLink a="www.facebook.com">
+                            <FooterLink to="www.facebook.com">
 
                             </FooterLink>
-                            <FooterLink a="www.facebook.com">
+                            <FooterLink to="www.facebook.com">
                                 Linkedin
                             </FooterLink>
-                            <FooterLink a="www.facebook.com">
+                            <FooterLink to="www.facebook.com">
                                 Facebook
                             </FooterLink>
                             
@@ -67,22 +82,52 @@ const Footer = () => {
                             <FooterLinkTitle>
                                 About me
                             </FooterLinkTitle>
-                            <FooterLink a="www.facebook.com">
+                            <FooterLink to="www.facebook.com">
                                 Github
                             </FooterLink>
-                            <FooterLink a="www.facebook.com">
+                            <FooterLink to="www.facebook.com">
 
                             </FooterLink>
-                            <FooterLink a="www.facebook.com">
+                            <FooterLink to="www.facebook.com">
                                 Linkedin
                             </FooterLink>
-                            <FooterLink a="www.facebook.com">
+                            <FooterLink to="www.facebook.com">
                                 Facebook
                             </FooterLink>
                             
                         </FooterLinkItems>
                     </FooterLinksWrapper>
                 </FooterLinksContainer>
+                <SocialMedia>
+                    <SocialMediaWrap>
+                        <SocialLogo to= "/">
+                            Octavio
+                        </SocialLogo>
+                        <WebsiteRights>Octavio Molina © {new Date().getFullYear()}  All rights reserved </WebsiteRights>
+                        <SocialIcons>
+                            <SocialIconLink href="/"
+                            target="_blank"
+                            aria-label="Github">
+                                <FaGithub/>
+                            </SocialIconLink>
+                            <SocialIconLink href="/"
+                            target="_blank"
+                            aria-label="Linkedin">
+                                <FaLinkedin/>
+                            </SocialIconLink>
+                            <SocialIconLink href="/"
+                            target="_blank"
+                            aria-label="Instagram">
+                                <FaInstagram/>
+                            </SocialIconLink>
+                            <SocialIconLink href="www.facebook.com"
+                            target="_blank"
+                            aria-label="Facebook">
+                                <FaFacebook/>
+                            </SocialIconLink>
+                        </SocialIcons>
+                    </SocialMediaWrap>
+                </SocialMedia>
             </FooterWrap>
         </FooterContainer>
     )
