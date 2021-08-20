1680520c1d7f18390d449fd04a168d9f12662fa7
Jose Octavio Molina Nava <jose.octavio@berkeley.edu>
2021-08-20 16:18:26 -0500

    checking

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ function App() {
     <Router>
       <Switch>
         <Route path='/' component= {Home} exact/>
-        <Route path='/signin' component={SigninPage} exact/>
       </Switch>
     </Router>
   );
