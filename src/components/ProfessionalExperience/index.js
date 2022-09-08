dc6fa5f7eed908b108dcf93cdde4d7477d5bf704
Jose Octavio Molina Nava <jose.octavio@ciencias.unam.mx>
2022-09-08 01:20:05 -0500

    removing comments from past comments on the code

--- src/components/ProfessionalExperience/index.js
+++ src/components/ProfessionalExperience/index.js
@@ -13,14 +13,12 @@ import {
 import niq_logo from "../../images/niq_logo.png";
 import orchata_logo from "../../images/orchata_logo.png";
 import hpe_logo from "../../images/hpe_logo.png";
-
+//Included logos from other companies, deleting unnecessary comments from past versions of the code.
 import { ModalUnmuted } from "../ModalUnmuted/ModalUnmuted.js";
 import { Modal } from "../Modal/Modal.js";
 import { nielseniq, orchata, hpe } from "../Modal/Data";
 
 const ProfessionalExperience = () => {
-  //hi
-  //this is a comment
   const [showModal, setShowModal] = useState(false);
   const [showModalU, setShowModalU] = useState(false);
 
